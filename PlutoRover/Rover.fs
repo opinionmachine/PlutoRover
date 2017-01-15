98175bb4332318198cbc130f77edc5513b005d02
Rikard Ottosson <hrillo@hotmail.com>
2017-01-15 11:15:53 +0000

    Second test - go backward

--- PlutoRover/Rover.fs
+++ PlutoRover/Rover.fs
@@ -4,8 +4,11 @@ type Rover(x0, y0 ,d0) =
     let mutable x = x0
     let mutable y = y0
     let mutable d = d0
-    member this.Move(commands) = 
-        y <- y + 1
+    member this.Move(commands: string) = 
+        if commands.[0] = 'F' then
+            y <- y + 1
+        else
+            y <- y - 1
         (x, y, d)
         
         
--- PlutoRoverTests/RoverTests.fs
+++ PlutoRoverTests/RoverTests.fs
@@ -13,4 +13,14 @@ type RoverTests() =
         Assert.That(x, Is.EqualTo(0));
         Assert.That(y, Is.EqualTo(1));
         Assert.That(d, Is.EqualTo('N'));
-        
+    
+    [<Test>]    
+    member this.Can_move_backward() = 
+        let r = Rover(1,1,'N')
+        let pos = r.Move("B")
+        let x,y,d = pos
+        Assert.That(x, Is.EqualTo(1));
+        Assert.That(y, Is.EqualTo(0));
+        Assert.That(d, Is.EqualTo('N'));
+
+
