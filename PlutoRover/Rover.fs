301ed331b82a113f73e0911736ed7ec6e187b828
Rikard Ottosson <hrillo@hotmail.com>
2017-01-15 10:19:33 +0000

    Initial commit - first test

---
+++ PlutoRover/Rover.fs
@@ -0,0 +1,11 @@
+﻿namespace PlutoRover
+
+type Rover(x0, y0 ,d0) = 
+    let mutable x = x0
+    let mutable y = y0
+    let mutable d = d0
+    member this.Move(commands) = 
+        y <- y + 1
+        (x, y, d)
+        
+        
---
+++ PlutoRoverTests/RoverTests.fs
@@ -0,0 +1,16 @@
+﻿namespace PlutoRoverTests
+open NUnit.Framework
+open PlutoRover
+
+[<TestFixture>]
+type RoverTests() = 
+    
+    [<Test>]
+    member this.Can_move_forward() = 
+        let r = Rover(0,0,'N')
+        let pos = r.Move("F")
+        let x,y,d = pos
+        Assert.That(x, Is.EqualTo(0));
+        Assert.That(y, Is.EqualTo(1));
+        Assert.That(d, Is.EqualTo('N'));
+        
